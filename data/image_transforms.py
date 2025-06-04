c0e3f8dee30d4c47fb9eee83f3f49511834a3678
Wenrui Huang <75226042+henwenrou@users.noreply.github.com>
2025-06-05 02:31:24 +0800

    Fix import of Sequence for Python 3.11

--- data/image_transforms.py
+++ data/image_transforms.py
@@ -4,7 +4,7 @@ Image transforms functions for data augmentation
 Credit to Dr. Jo Schlemper
 """
 
-from collections import Sequence
+from collections.abc import Sequence
 import cv2
 import numpy as np
 import scipy
